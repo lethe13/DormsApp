f7256d192d3f3a9ca77efd4248fb28303ccfeda1
300693183 <300693183@PR-A315-04>
2017-01-12 19:54:32 -0500

    Add project files.

---
+++ DormsApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DormsApp.Startup))]
+namespace DormsApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
